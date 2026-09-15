a9d1d59236e57c3682cf6851c4951fcc520fb768
canerildes <canerildes@gmail.com>
2026-09-15 11:58:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // TUI BLUE Palm Garden Eğitim Portalı - ORTAK OneSignal / PWA yapılandırması
 // Güvenlik + Eğitim aynı OneSignal uygulamasını kullanır.
 window.TBPG_EDUCATION_CONFIG = {
-  APP_URL: "https://script.google.com/macros/s/AKfycbz41b8SI886o5_JMnTOF38AK2JwOA5zTtuUKVOOVH_eUshCagQJOytmdrhoThpQehW9/exec",
+  APP_URL: "https://script.google.com/macros/s/AKfycbz41b8SJ886o5_JMnTOF38AK2JwOA5zTtuUKVOOVH_eUshCagQJOytmdrhoThpQehW9/exec",
   ONESIGNAL_APP_ID: "42b7220b-661b-45f3-907c-e54b9ea99aec",
   ONESIGNAL_SAFARI_WEB_ID: "web.onesignal.auto.166712e5-2aee-41ea-b95e-8df3d8ab1f28",
   ONESIGNAL_WORKER_PATH: "/e-learning/service-worker.js",
