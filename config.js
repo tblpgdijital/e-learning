8237d6d30059b5fe62d97c02284613a717a2def2
canerildes <canerildes@gmail.com>
2026-09-15 11:16:10 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // APP_URL: mevcut Eğitim Portalı Apps Script dağıtımınızın /exec adresini buraya yapıştırın.
 // Örnek biçim: https://script.google.com/macros/s/XXXXXXXX/exec
 window.TBPG_EDUCATION_CONFIG = {
-  APP_URL: "https://script.google.com/macros/s/AKfycbz41b8SJ886o5_JMnTOF38AK2JwOA5zTtuUKVOOVH_eUshCagQJOytmdrhoThpQehW9/exec",
+  APP_URL: "BURAYA_MEVCUT_EGITIM_PORTALI_EXEC_URL",
   ONESIGNAL_APP_ID: "42b7220b-661b-45f3-907c-e54b9ea99aec",
   ONESIGNAL_WORKER_PATH: "/e-learning/service-worker.js",
   ONESIGNAL_WORKER_SCOPE: "/e-learning/",
